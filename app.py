19a0c0893f536743df72eae571f6a9728184da71
Elizaveta15 <75948717+Elizaveta15@users.noreply.github.com>
2023-12-18 13:37:01 +0300

    finish

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_apscheduler import APScheduler
 
-from logging import  FileHandler, INFO, DEBUG
+from logging import  FileHandler, DEBUG
 
 from bd import session
 from Entity.User import User
